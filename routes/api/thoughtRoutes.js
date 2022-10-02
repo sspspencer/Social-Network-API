9113ba43e1b7ab26a9e7efc6a6e797f9d895e984
Spencer Pichette <ssptuningperformance@gmail.com>
2022-10-02 16:47:24 -0400

    fixed bugs

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -23,7 +23,7 @@ router
 // http://localhost:3001/api/thoughts/:thoughtId/reactions
 router.route("/:thoughtId/reactions").post(createReaction);
 
-// http://localhost:3001/api/thoughts/:thoughtId/reactions/:reactiongId
+// http://localhost:3001/api/thoughts/:thoughtId/reactions/:reactionId
 router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
 
 module.exports = router;
