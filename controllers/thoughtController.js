de3c2e5ef6c0d4e4d5639ff583b1e2f99f986285
Spencer Pichette <ssptuningperformance@gmail.com>
2022-10-02 15:38:51 -0400

    Thought/reactions controllers set up

---
+++ controllers/thoughtController.js
@@ -0,0 +1,92 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+  getThoughts(req, res) {
+    Thought.find({})
+      .then((thoughtData) => res.json(thoughtData))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  getThoughtById(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((thoughtData) => {
+        !thoughtData
+          ? res.status(404).json({ message: "No Thought with this Id" })
+          : res.json(thoughtData);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((thoughtData) =>
+        !thoughtData
+          ? res.status(404).json({ message: "No User with this Id" })
+          : res.json(thoughtData)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, New: true }
+    )
+      .then((userData) =>
+        !userData
+          ? res.status(404).json({ message: "No thought with this Id" })
+          : res.json(userData)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((thoughtData) =>
+        !thoughtData
+          ? res.status(404).json({ message: "No thought with this Id" })
+          : User.findOneAndUpdate(
+              { thoughts: req.params.thoughtId },
+              { $pull: { thoughts: req.params.thoughtId } },
+              { new: true }
+            )
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  createReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((thoughtData) =>
+        !thoughtData
+          ? res.status(404).json({ message: "No thought with this Id!" })
+          : res.json(thoughtData)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  deleteReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } }
+    )
+      .then((thoughtData) =>
+        !thoughtData
+          ? res.status(404).json({ message: "No thoughts with this Id" })
+          : res.json(thoughtData)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
--- models/Thought.js
+++ models/Thought.js
@@ -40,6 +40,12 @@ const thoughtSchema = new Schema(
       minlength: 1,
       maxlength: 280,
     },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
     username: {
       type: String,
       required: true,
